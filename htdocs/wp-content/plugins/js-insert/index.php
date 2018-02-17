118f328edcdd313ed75ea15d4a9e01481a56b0e9
Z Jonesy <Z-Jonesy@users.noreply.github.com>
2018-02-17 13:33:41 +0100

    add: add_my_style

--- htdocs/wp-content/plugins/js-insert/index.php
+++ htdocs/wp-content/plugins/js-insert/index.php
@@ -10,16 +10,38 @@ License: MIT
 Text Domain: JSinsert
 */
 
-//JS fájlok beszúrása a frontend oldalra
+function add_my_scripts()
+{
 
-
-function add_my_scripts(){
+    // az ajax handler script beszúrása az oldal láblécébe
     wp_enqueue_script(
         'ajax_handler',
         plugins_url('js-insert/js/ajax-handler.js'),
         ['jquery'],
         date('YmdHis'),
         true
-        );
+    );
+
+    // PHP változók hozzáadása a scripthez
+    wp_localize_script(
+        'ajax-handler',
+        'ajaxOptions',
+        [
+            'ajaxurl' => admin_url('admin-ajax.php'),
+            'actionName' => 'it_ajax'
+        ]
+    );
 }
-add_action( 'wp_enqueue_scripts', 'add_my_scripts' );
+
+add_action('wp_enqueue_scripts', 'add_my_scripts');
+
+// style beszúrása
+function add_my_style() {
+    wp_enqueue_style(
+        'page-style',
+        plugins_url('js-insert/css/page-css.css'),
+        array(),
+        date('YmdHis')
+    );
+}
+add_action('wp_enqueue_style', 'add_my_style');
