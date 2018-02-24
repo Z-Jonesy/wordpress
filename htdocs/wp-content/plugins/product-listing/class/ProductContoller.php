df4584e7ce737abd6aff9d01c1d1658b486650da
Z Jonesy <Z-Jonesy@users.noreply.github.com>
2018-02-24 19:23:29 +0100

    add: extend CRUD basic functions

--- htdocs/wp-content/plugins/product-listing/class/ProductContoller.php
+++ htdocs/wp-content/plugins/product-listing/class/ProductContoller.php
@@ -6,39 +6,72 @@
  * Time: 13:04
  */
 
-class ProductController {
+class ProductController
+{
 
     private $method;
     private $model;
     private $methodPairs = array(
-      'get' => 'get',
-      'post' => 'update',
-      'put' =>'insert',
-      'delete' => 'delete'
+        'get' => 'get',
+        'post' => 'update',
+        'put' => 'insert',
+        'delete' => 'delete'
     );
 
     function __construct()
     {
         $this->model = new ProductModel;
+
+        // metódus meghatározása
         $this->getMethod();
+
+        // input adatok összegyűjtése
         $input = $this->getInput();
-        echo json_encode($input);
-        wp_die();
+
+        // modell metódus hívása
+        switch ($this->method) {
+            case 'get':
+                if (isset($input->id)) {
+                    exit(json_encode($input));
+                    $result = $this->model->get($input->id);
+                } else {
+                    $result = $this->model->get();
+                }
+                break;
+            default:
+                break;
+        }
+
         $result = call_user_func(
             array(
                 $this->model,
                 $this->methodPairs[$this->method]
             )
         );
+
         echo json_encode($result);
         wp_die();
     }
 
-    private function getMethod(){
-        $this->method = strtolower( $_SERVER['REQUEST_METHOD']);
+    private function getMethod()
+    {
+        $this->method = strtolower($_SERVER['REQUEST_METHOD']);
     }
 
-    private function getInput() {
-        return file_get_contents( 'php://input');
+    private function getInput()
+    {
+        $input = file_get_contents('php://input');
+        if (strlen($input) < 1) {
+            $input = new stdClass();
+        } else {
+            $input = json_decode($input);
+        }
+        $input = json_decode($input);
+        if (count($_GET) > 0) {
+            foreach ($_GET as $key => $value) {
+                $input->{$key} = $value;
+            }
+        }
+
     }
 }
