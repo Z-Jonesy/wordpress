e2845c1de7567a2e888b1679f2123e60593439a6
Z Jonesy <Z-Jonesy@users.noreply.github.com>
2018-02-24 12:50:42 +0100

    add: add plugin product-listing and define class productModel

---
+++ htdocs/wp-content/plugins/product-listing/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Product lister
+Plugin URI: http://zbits.hu/
+Description: Listing and editing
+Version: 0.0.1
+Author: Zozi
+Author URI: http://zbits.hu
+License: MIT
+Text Domain: Product lister
+*/
+
+//Product model
+include 'class/ProductModel.php';
+//Controller
+
+//
